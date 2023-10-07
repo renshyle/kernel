5376aa30e8c9406fdf541ec59d2e679c7f8a8609
renshyle <renshyle@proton.me>
2023-10-07 11:30:39 +0300

    fix sfmask msr, interrupts now actually cleared on syscall

--- kernel/src/msr.h
+++ kernel/src/msr.h
@@ -5,7 +5,7 @@
 
 #define MSR_STAR 0xc0000081
 #define MSR_LSTAR 0xc0000082
-#define MSR_SFMASK 0xc0000085
+#define MSR_SFMASK 0xc0000084
 #define MSR_IA32_EFER 0xc0000080
 
 void wrmsr(uint32_t msr, uint64_t value);
